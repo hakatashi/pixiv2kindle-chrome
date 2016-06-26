df129f7719e13619b2b9a4c45e1a1c32a98f9ec9
Koki Takahashi <hakatasiloving@gmail.com>
2016-06-26 19:06:30 +0900

    Implement options page

--- app/scripts/options.babel.js
+++ app/scripts/options.babel.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
+document.addEventListener('DOMContentLoaded', () => {
+    // Restore saved options
+    chrome.storage.sync.get({
+        auth: '',
+    }, items => {
+        document.getElementById('auth').value = items.auth;
+    });
+
+    document.getElementById('save').addEventListener('click', () => {
+        chrome.storage.sync.set({
+            auth: document.getElementById('auth').value,
+        }, () => {
+            const statusEl = document.getElementById('status');
+            statusEl.textContent = 'Option saved.';
+            setTimeout(() => {
+                statusEl.textContent = '';
+            }, 1000);
+        })
+    });
+});
