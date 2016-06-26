8dfaf5e3bffe446e166a1acc6e6dac8172efbfc6
Koki Takahashi <hakatasiloving@gmail.com>
2016-06-26 17:53:32 +0900

    Prettify window

--- app/scripts/popup.babel.js
+++ app/scripts/popup.babel.js
@@ -3,6 +3,16 @@
 const request = require('request');
 const parse = require('csv-parse');
 
+const pushEvent = (event) => {
+	const li = document.createElement('li');
+	li.textContent = event.event;
+	document.getElementsByClassName('logs')[0].appendChild(li);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	pushEvent({event: 'Script Loaded'});
+});
+
 const parser = parse();
 
 request.post('https://api.hakatashi.com/pixiv2kindle/publish').pipe(parser);
@@ -11,9 +21,6 @@ parser.on('readable', () => {
 	let record;
 	while (record = parser.read()) {
 		const event = JSON.parse(record[0]);
-
-		const p = document.createElement('p');
-		p.textContent = event.event;
-		document.body.appendChild(p);
+		pushEvent(event);
 	}
 });
