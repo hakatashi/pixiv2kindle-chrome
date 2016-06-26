8268b29edffe4b00e3c81de82fce69950c3dc584
Koki Takahashi <hakatasiloving@gmail.com>
2016-06-26 19:22:37 +0900

    Implement background page

--- app/scripts/background.babel.js
+++ app/scripts/background.babel.js
@@ -1,11 +1,27 @@
 'use strict';
 
+const url = require('url');
+
 chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
+    console.log('previousVersion', details.previousVersion);
 });
 
-chrome.tabs.onUpdated.addListener(tabId => {
-  chrome.pageAction.show(tabId);
-});
+chrome.tabs.onUpdated.addListener((id, info) => {
+    if (info.url) {
+        const pageUrl = url.parse(info.url, true);
 
-console.log('\'Allo \'Allo! Event Page for Page Action');
+        if (
+            (
+                // PC/touch novel page
+                (pageUrl.hostname === 'www.pixiv.net' || pageUrl.hostname === 'touch.pixiv.net')
+                && pageUrl.pathname === '/novel/show.php'
+            ) || (
+                // whitecube
+                pageUrl.hostname === 'www.pixiv.net'
+                && pageUrl.pathname.match(/^(\/whitecube)?\/novel\/\d+$/)
+            )
+        ) {
+            chrome.pageAction.show(id);
+        }
+    }
+});
